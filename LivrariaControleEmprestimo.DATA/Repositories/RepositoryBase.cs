485b8b97866155b9c5c6d04ff3b46bad11b84174
eddyangelop <eddysoullive@gmail.com>
2023-05-04 20:54:17 -0300

    Isntalado pacotes Nuget e Criado class RepositoryBase

---
+++ LivrariaControleEmprestimo.DATA/Repositories/RepositoryBase.cs
@@ -0,0 +1,79 @@
+﻿using LivrariaControleEmprestimo.DATA.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static LivrariaControleEmprestimo.DATA.Interfaces.IRepositoryModel;
+
+namespace LivrariaControleEmprestimo.DATA.Repositories
+{   
+
+    public class RepositoryBase<T> : IRepositoryModel<T>, IDisposable where T : class
+    {
+        protected ControleEmprestimoLivroContext _Contexto;
+        public bool _SaveChanges = true;
+
+        public RepositoryBase(bool SaveChanges) {
+        _SaveChanges = SaveChanges;
+            _Contexto = new ControleEmprestimoLivroContext();
+        }
+
+
+        public T Alterar(T objeto)
+        {
+            _Contexto.Entry(objeto).State = EntityState.Modified;
+
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+            return objeto;
+        }
+
+        public void Dispose()
+        {
+            _Contexto.Dispose();
+        }
+
+        public void Excluir(T objeto)
+        {
+            _Contexto.Set<T>().Remove(objeto);
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+        }
+
+        public void Excluir(params object[] variavel)
+        {
+            var obj = selecionarPK(variavel);
+            Excluir(obj);
+        }
+
+        public T Incluir(T objeto)
+        {
+            _Contexto.Set<T>().Add(objeto);
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+            return objeto;
+        }
+
+        public void SaveChanges()
+        {
+            _Contexto.SaveChanges();
+        }
+
+        public T selecionarPK(params object[] variavel)
+        {
+            return _Contexto.Set<T>().Find(variavel);
+        }
+
+        public List<T> SelecionarTodos()
+        {
+            return _Contexto.Set<T>().ToList();
+        }
+    }
+}
+
