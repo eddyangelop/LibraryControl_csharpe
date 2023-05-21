366c28ad0be68638c692d0f5449b7e920368cf5c
eddyangelop <eddysoullive@gmail.com>
2023-05-20 23:23:34 -0300

    criada View List de EmprestimoController e configurando models no layout das paginas para acesso de cada View.

--- LivrariaControleEmprestimo.WEB/Controllers/EmprestimosController.cs
+++ LivrariaControleEmprestimo.WEB/Controllers/EmprestimosController.cs
@@ -7,11 +7,28 @@ namespace LivrariaControleEmprestimo.WEB.Controllers
 {
     public class EmprestimosController : Controller
     {
-        private LivroClienteEmprestimoService _service = new LivroClienteEmprestimoService();
+        private LivroClienteEmprestimoService oEmprestimoService = new LivroClienteEmprestimoService();
         public IActionResult Index()
         {
-            List<VwLivroClienteEmprestimo> oListVwLivroClienteEmprestimo = _service.oRepositoryVmClienteEmprestimo.SelecionarTodos();
+            List<VwLivroClienteEmprestimo> oListVwLivroClienteEmprestimo = oEmprestimoService.oRepositoryVmClienteEmprestimo.SelecionarTodos();
                 return View(oListVwLivroClienteEmprestimo);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VwLivroClienteEmprestimo model )
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View();
+            }
+            oEmprestimoService.oRepositoryVmClienteEmprestimo.Incluir(model);
+
+            return RedirectToAction("Index");
         }
     }
 }
